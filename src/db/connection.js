d8faeaa7ddc30c9b973fc373e0dbf93a1167a2e6
John Parkhurst <jparkhurst120@gmail.com>
2022-12-22 00:58:12 -0500

    Mongo -> SQL

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,26 @@
-const monk = require('monk');
-//This line isnt working below
+const mysql = require("mysql");
+
 let dbUrl = process.env.DB_URL;
 
 if (process.env.NODE_ENV === 'test') {
     dbUrl = process.env.TEST_DB_URL;
 }
-const db = monk(dbUrl);
 
-module.exports = db;
+//dburl & port is undefined for some reason
+
+
+const connection = mysql.createConnection({
+    host: dbUrl,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB
+});
+console.log("test1")
+
+// open the MySQL connection
+connection.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
+console.log("test2")
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -6,20 +6,38 @@ const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares');
 
 require('dotenv').config({ path: '../.env' });
-
 const schema = require('./db/schema');
 console.log("WORKS")
 const db = require('./db/connection');
-console.log("DB WORKS")
-const employees = db.get('Whitelist');
-
-
+console.log("DB WORKS");
+//const whitelist = db.get('Whitelist');
 const app = express();
-const monk = require('monk');
+const mysql = require("mysql");
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+/* Get all whitelists */
+app.get('/', async (req, res, next) => {
+    console.log("RECEIVED")
+
+    try {
+        console.log("FIRST")
+        //const allWhitelists = await whitelist.find({}, 'identifier').then((docs) => {});
+
+        console.log("SECOND")
+        res.json("a");
+    } catch(error) {
+        next(error);
+    }
+});
+
+app.get('/health', async (req, res, next) => {
+    console.log("Health Check");
+    res.json({message: 'Server is up and running.'});
+});
+
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
