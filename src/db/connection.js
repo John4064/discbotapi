e8bfc1c0a62f90748b41d5b59d34e5ac9e2c4204
John Parkhurst <jparkhurst120@gmail.com>
2022-12-29 20:31:34 -0500

    Refactor & Err Handling Load Up
    Error handling for loading up the api if the db doesnt connect itll wait a minute then retry

--- src/db/connection.js
+++ src/db/connection.js
@@ -8,17 +8,37 @@ if (process.env.NODE_ENV === 'test') {
 
 //dburl & port is undefined for some reason
 
-const connection = mysql.createConnection({
+var db_config = {
     host: "10.0.1.27",
-    user: "root",
-    password: "Panda1234",
-    database: "safapi"
-});
+        user: "root",
+        password: "Panda1234",
+        database: "safapi"
+};
+var connection;
 
-// open the MySQL connection
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-});
+function handleDisconnect(){
+    connection = mysql.createConnection(db_config);
+
+    // open the MySQL connection
+    connection.connect(err => {
+        if (err){
+            console.log("Error when connecting to the db:",err);
+            setTimeout(handleDisconnect,60000);
+        }else{
+            console.log("Successfully connected to the database.");
+        }
+        connection.on('error', function(err) {
+            console.log('db error', err);
+            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+                handleDisconnect();                         // lost due to either server restart, or a
+            } else {                                      // connnection idle timeout (the wait_timeout
+                throw err;                                  // server variable configures this)
+            }
+        })
+
+    });
+}
+handleDisconnect();
+
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -6,36 +6,11 @@ const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares');
 
 require('dotenv').config({ path: '../.env' });
-const schema = require('./db/schema');
-const db = require('./db/connection');
-//const whitelist = db.get('Whitelist');
 const app = express();
-const mysql = require("mysql");
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-/* Get all whitelists */
-// app.get('/', async (req, res, next) => {
-//     let query = "SELECT * FROM Whitelist";
-//     db.query(query,(err,sqlResult)=>{
-//         if(err){
-//             console.error("Error with SQL Request");
-//             next(err);
-//         }
-//         res.json(sqlResult);
-//     });
-// });
-//
-// /*Add to Whitelist*/
-//
-//
-//
-// app.get('/health', async (req, res, next) => {
-//     console.log("Health Check");
-//     res.json({message: 'Server is up and running.'});
-// });
-//
 
 require("./routes/routes.js")(app);
 const port = process.env.PORT || 8080;
